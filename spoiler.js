c43c3e964100ffb8b2e6409da2847afb1067e045
jcouard <j.couard@laposte.net>
2016-10-23 14:01:54 +0200

    Add files via upload

---
+++ spoiler.js
@@ -0,0 +1,5 @@
+var button = document.querySelector('.spoiler button');
+button.addEventListener('click', function () {
+    this.nextElementSibling.classList.add('visible');
+    this.parentNode.removeChild(this);
+});
